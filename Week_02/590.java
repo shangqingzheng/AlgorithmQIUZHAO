d972a880bedf238bd539a8d7c9bdab60d3eeb1cb
sinbadshang <sinbadshang@tencent.com>
2020-07-22 17:11:40 +0800

    第二周 作业 590 多叉树后序遍历

---
+++ Week_02/590.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        //利用栈。链表
+        LinkedList<Node> stack = new LinkedList<Node>();//栈
+        LinkedList<Integer> res = new LinkedList<Integer>();//结果链表。一直往链表头节点插入。
+        //处理root为null的情况
+        if (root == null) {
+            return res;
+        }
+        //初始化栈
+        stack.add(root);
+
+        //迭代
+        while(!stack.isEmpty()) {//迭代条件是栈不为空，如果栈为空，则遍历完成
+            //先从栈里面取出来最新放进去的元素。
+            Node node = stack.pollLast();
+            //把栈当前取出来的元素放入链表头。
+            res.addFirst(node.val);
+
+            //如果当前节点还有子节点，则把子节点顺序加入到栈中。
+            for(Node item : node.children) {
+                if (item != null) {
+                    stack.add(item);
+                }
+            }
+        }
+        return res;
+    }
+}
+
